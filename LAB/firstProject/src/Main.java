d7e7f8793e8a63b28228c4208cc598d3d575f3f2
MarinCervinschi <marincervinschi3@gmail.com>
2024-02-27 15:05:20 +0100

    head

---
+++ LAB/firstProject/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.print("Hello and welcome!\n");
+        int k = 55;
+
+
+
+        for (int i = 1; i <= 5; i++) {
+
+            System.out.println("i = " + i);
+        }
+    }
+}
