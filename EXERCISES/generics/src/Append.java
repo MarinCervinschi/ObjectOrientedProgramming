fd31296a25237a75e5f82954ba7e65d99249bb03
MarinCervinschi <marincervinschi3@gmail.com>
2024-03-04 19:16:11 +0100

    generics ex

---
+++ EXERCISES/generics/src/Append.java
@@ -0,0 +1,7 @@
+import java.util.List;
+
+public class Append {
+    public static <T> void append(List<T> dst, List<T> src) {
+        dst.addAll(src);
+    }
+}
---
+++ EXERCISES/generics/src/Divide.java
@@ -0,0 +1,5 @@
+public class Divide {
+    public static <T extends Number> double divide(T a, T b) {
+        return a.doubleValue() / b.doubleValue();
+    }
+}
