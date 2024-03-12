e85bdc1bd3eb6fdd2b1e9c3d0e90b4cf92e3cb78
MarinCervinschi <marincervinschi3@gmail.com>
2024-03-12 15:52:14 +0100

    oop ex

--- exercices/src/main/java/oop/basic/Letter.java
+++ exercices/src/main/java/oop/basic/Letter.java
@@ -1,17 +1,35 @@
 package oop.basic;
 
+/**
+ * A class for authoring a simple letter.
+ */
 public class Letter {
     String from;
     String to;
     StringBuilder lines = new StringBuilder();
+
+    /**
+     * Set the names of the sender and the recipient.
+     * @param from the sender.
+     * @param to the recipient.
+     */
     public Letter(String from, String to) {
         this.from = from;
         this.to = to;
     }
 
+    /**
+     * A method to add a line of text to the body of the letter.
+     * @param line the line to be added.
+     */
     public void addLine(String line) {
         lines.append(line).append("\n");
     }
+
+    /**
+     *
+     * @return the entire text of the letter;
+     */
     public String getText() {
         String start = "Dear " + to + ":\n\n";
         String end = "\nSincerely,\n\n" + from;
