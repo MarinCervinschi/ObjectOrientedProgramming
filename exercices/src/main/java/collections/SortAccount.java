67cb19e4498580753410e5cee66fdc93376a6bab
MarinCervinschi <marincervinschi3@gmail.com>
2024-03-20 19:22:18 +0100

    collections

---
+++ exercices/src/main/java/collections/SortAccount.java
@@ -0,0 +1,41 @@
+package collections;
+
+import java.time.LocalDate;
+import java.util.Comparator;
+import java.util.List;
+
+public class SortAccount {
+    public static class Account {
+        double amount;
+        double interestRate;
+        LocalDate duePayment;
+
+        public Account(double amount, double interestRate, LocalDate duePayment) {
+            this.amount = amount;
+            this.interestRate = interestRate;
+            this.duePayment = duePayment;
+        }
+
+        public double getAmount() {
+            return amount;
+        }
+
+        public double getInterestRate() {
+            return interestRate;
+        }
+
+        public LocalDate getDuePayment() {
+            return duePayment;
+        }
+
+    }
+    public static void sortByAmount(List<Account> accounts) {
+        accounts.sort(Comparator.comparingDouble(Account::getAmount));
+    }
+    public static void sortByInterestRate(List<Account> accounts) {
+        accounts.sort(Comparator.comparingDouble(Account::getInterestRate));
+    }
+    public static void sortByDuePayment(List<Account> accounts) {
+        accounts.sort(Comparator.comparing(Account::getDuePayment));
+    }
+}
