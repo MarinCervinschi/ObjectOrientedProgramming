a573e8f5e59b6ea52bd897d93e89de2a7a5f676d
MarinCervinschi <marincervinschi3@gmail.com>
2024-03-20 18:57:09 +0100

    test for getters and setters

--- exercices/src/test/java/oop/library/PersonTest.java
+++ exercices/src/test/java/oop/library/PersonTest.java
@@ -6,16 +6,28 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 class PersonTest {
+        Person p1 = new Person("Hello", "World", "ID123");
+        Person p2 = new Person("Hello", "World", "ID123");
+        Person p3 = new Person("Hi", "World", "ID123");
 
     @Test
     void equalsHashCode() {
-        Person p1 = new Person("ID123", "Hello", "World");
-        Person p2 = new Person("ID123", "Hello", "World");
-        Person p3 = new Person("ID123", "Hi", "World");
 
         assertEquals(p1, p2);
         assertNotEquals(p1, p3);
         assertEquals(p1.hashCode(), p2.hashCode());
         assertNotEquals(p1.hashCode(), p3.hashCode());
+    }
+
+    @Test
+    void gettersAndSetters() {
+
+        p1.setName("Mario");
+        p1.setLastname("Rossi");
+        p1.setId("ID122");
+
+        assertEquals("Mario", p1.getName());
+        assertEquals("Rossi", p1.getLastname());
+        assertEquals("ID122", p1.getId());
     }
 }
--- exercices/src/test/java/oop/library/RentTest.java
+++ exercices/src/test/java/oop/library/RentTest.java
@@ -12,12 +12,12 @@ class RentTest {
     void isExpired() {
         Rent r1 = new Rent(
                 new Book("Effective Java", 2019, 1350),
-                new Person("ID1456", "Nicola", "Bicocchi"),
+                new Person("Nicola", "Bicocchi", "ID1456"),
                 LocalDate.of(2022, 3, 1),
                 LocalDate.of(2022, 7, 1));
         Rent r2 = new Rent(
                 new Book("Big Java", 2019, 1350),
-                new Person("ID1456", "Nicola", "Bicocchi"),
+                new Person("Nicola", "Bicocchi", "ID1456"),
                 LocalDate.of(2023, 3, 1),
                 LocalDate.of(2023, 7, 1));
         assertTrue(r1.isExpired(LocalDate.of(2023, 4, 1)));
@@ -28,22 +28,39 @@ class RentTest {
     void equalsHashCode() {
         Rent r1 = new Rent(
                 new Book("Big Java", 2019, 1350),
-                new Person("ID1456", "Nicola", "Bicocchi"),
+                new Person("Nicola", "Bicocchi", "ID1456"),
                 LocalDate.of(2023, 3, 1),
                 LocalDate.of(2023, 7, 1));
         Rent r2 = new Rent(
                 new Book("Big Java", 2019, 1350),
-                new Person("ID1456", "Nicola", "Bicocchi"),
+                new Person("Nicola", "Bicocchi", "ID1456"),
                 LocalDate.of(2023, 3, 1),
                 LocalDate.of(2023, 7, 1));
         Rent r3 = new Rent(
                 new Book("Big Java", 2019, 1350),
-                new Person("ID1456", "Nicola", "Bicocchi"),
+                new Person("Nicola", "Bicocchi","ID1456"),
                 LocalDate.of(2022, 3, 1),
                 LocalDate.of(2022, 7, 1));
         assertEquals(r1, r2);
         assertNotEquals(r1, r3);
         assertEquals(r1.hashCode(), r2.hashCode());
         assertNotEquals(r1.hashCode(), r3.hashCode());
+    }
+
+    @Test
+    void gettersAndSetters() {
+        Rent r1 = new Rent(
+                new Book("Effective Java", 2019, 1350),
+                new Person("Nicola", "Bicocchi", "ID1456"),
+                LocalDate.of(2022, 3, 1),
+                LocalDate.of(2022, 7, 1));
+        r1.setBegin(LocalDate.of(2024, 3, 20));
+        r1.setEnd(LocalDate.of(2024, 3, 25));
+
+        Book b1 = new Book("Big Java", 2019, 1350);
+        r1.setItem(b1);
+        assertEquals(LocalDate.of(2024, 3, 20), r1.getBegin());
+        assertNotEquals(LocalDate.of(2024, 3, 22), r1.getEnd());
+        assertEquals(b1, r1.getItem());
     }
 }
